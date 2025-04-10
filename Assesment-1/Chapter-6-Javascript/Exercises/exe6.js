13cfefe967e0ac61a68d5888443c36d8f8b9e77f
shadytwt <idkiiwhoh@gmail.com>
2025-04-10 17:30:17 +0400

    .

--- Assesment-1/Chapter-6-Javascript/Exercises/exe6.js
+++ Assesment-1/Chapter-6-Javascript/Exercises/exe6.js
@@ -1,12 +1,9 @@
-// Function to calculate total petrol cost
 function calculateTotal() {
-    // Get input values
+
     const costPerLitre = parseFloat(document.getElementById('costPerLitre').value);
     const litresPurchased = parseFloat(document.getElementById('litresPurchased').value);
 
-    // Calculate total cost
     const totalCost = costPerLitre * litresPurchased;
 
-    // Display the total cost formatted to 2 decimal places
     document.getElementById('totalCost').innerText = `Total Cost: AED ${totalCost.toFixed(2)}`;
 }
